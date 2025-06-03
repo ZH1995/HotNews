a00036203dfda880c7e91df4b09f57bd1149b15b
zh1995 <rwzsjfm@qq.com>
2025-06-03 10:32:50 +0800

    榜单聚合页面

--- news/models.py
+++ news/models.py
@@ -5,11 +5,13 @@ class NewsArticle(models.Model):
     title = models.CharField(max_length=255)  # 数据库中列名就是'title'
     url = models.CharField(max_length=255)  # 之前模型中缺少这个字段
     hot_rank = models.IntegerField(null=True, blank=True)  # 数据库中允许为NULL
+    source = models.IntegerField()  # 添加数据源字段
+    batch_timestamp = models.DateTimeField(null=True, blank=True)  # 批次时间戳字段
     created_at = models.DateTimeField(auto_now_add=True)  # CURRENT_TIMESTAMP默认值
     
     def __str__(self):
         return self.title
     
     class Meta:
-        db_table = 'weibo_hot_search'
+        db_table = 'ranking'
         managed = False  # 告诉Django不要管理这个表的创建/修改/删除
